0caa76c085e49602fd8d8116a93718b0659ae2f9
Nicolas Espino <nicolasespino@wlan-t-25-193-dhcp.CSUChico.EDU>
2020-01-29 11:43:48 -0800

    Takes in Two intergers

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-
 using std::cin;
 using std::cout;
+using std::cerr;
+using std::endl;
 
 int main()
 {
+  int x;
+  int y;
+
     cout<<"Hi, please enter two numbers: ";
+
+    cin >> x >> y;
+
 
     return 0;
 }
